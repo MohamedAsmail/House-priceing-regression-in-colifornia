3b9c38125ba46917d02aa9e4838987208d195761
MohamedAsmail <108075485+MohamedAsmail@users.noreply.github.com>
2022-07-22 04:20:06 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -19,14 +19,14 @@ st.sidebar.header('please enter input')
 #function take input from user
 def user_input_features():
     #declaration for variables to hold the data
-    MedInc=st.sidebar.slider('MedInc',X.MedInc.min(),X.MedInc.max(),X.MedInc.mean())
-    HouseAge=st.sidebar.slider('HouseAge',float,X.HouseAge.min(),float,X.HouseAge.max(),float,X.HouseAge.mean())
-    AveRooms=st.sidebar.slider('AveRooms',float,X.AveRooms.min(),float,X.AveRooms.max(),float,X.AveRooms.mean()) 
-    AveBedrms=st.sidebar.slider('AveBedrms',float,X.AveBedrms.min(),float,X.AveBedrms.max(),float,X.AveBedrms.mean()) 
-    Population=st.sidebar.slider('Population',float,X.Population.min(),float,X.Population.max(),float,X.Population.mean()) 
-    AveOccup=st.sidebar.slider('AveOccup',float,X.AveOccup.min(),float,X.AveOccup.max(),float,X.AveOccup.mean())
-    Latitude=st.sidebar.slider('Latitude',float,X.Latitude.min(),float,X.Latitude.max(),float,X.Latitude.mean()) 
-    Longitude=st.sidebar.slider('Longitude',float,X.MedInc.min(),float,X.Longitude.max(),float,X.Longitude.mean())
+    MedInc=st.sidebar.slider('MedInc',float(X.MedInc.min()),float(X.MedInc.max()),float(X.MedInc.mean()))
+    HouseAge=st.sidebar.slider('HouseAge',float(X.HouseAge.min()),float(X.HouseAge.max()),float(X.HouseAge.mean()))
+    AveRooms=st.sidebar.slider('AveRooms',float(X.AveRooms.min()),float(X.AveRooms.max()),float(X.AveRooms.mean())) 
+    AveBedrms=st.sidebar.slider('AveBedrms',float(X.AveBedrms.min()),float(X.AveBedrms.max()),float(X.AveBedrms.mean())) 
+    Population=st.sidebar.slider('Population',float(X.Population.min()),float(X.Population.max()),float(X.Population.mean())) 
+    AveOccup=st.sidebar.slider('AveOccup',float(X.AveOccup.min()),float(X.AveOccup.max()),float(X.AveOccup.mean()))
+    Latitude=st.sidebar.slider('Latitude',float(X.Latitude.min()),float(X.Latitude.max()),float(X.Latitude.mean())) 
+    Longitude=st.sidebar.slider('Longitude',float(X.MedInc.min()),float(X.Longitude.max()),float(X.Longitude.mean()))
     #preparing the output of the function
     data={
         'MedInc':MedInc,
